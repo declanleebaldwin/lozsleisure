205812c63a5dfd8ce19da37ab0f2841658bd85d7
declan baldwin <dbaldwin4@boxfusionconsulting.com>
2019-07-02 19:33:30 +0100

    adding social media logos for footer

--- components/FooterPrismic.vue
+++ components/FooterPrismic.vue
@@ -1,15 +1,28 @@
 <template>
     <footer class="footer">
         <div class="content has-text-centered">
-            <p>
-                <strong>Bulma</strong> by
-                <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                <a
-                    href="http://opensource.org/licenses/mit-license.php"
-                >MIT</a>. The website content
-                is licensed
-                <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-            </p>
+            <a class="social-media-link" href="https://www.facebook.com/LozsLeisure/" target="_blank">
+                <img class="social-media-logo" src="~/assets/img/facebook-logo.svg" alt="facebook">
+            </a>
+            <a class="social-media-link" href="https://www.youtube.com/channel/UCwZq7eGqRMwY2gf7k9DcnXA" target="_blank">
+                <img class="social-media-logo" src="~/assets/img/youtube-logo.svg" alt="youtube">
+            </a>
+            <a class="social-media-link" href="https://www.instagram.com/lozsleisure/?hl=en" target="_blank">
+                <img class="social-media-logo" src="~/assets/img/instagram-logo.svg" alt="instagram">
+            </a>
+            <a class="social-media-link" href="https://twitter.com/lozsleisure?lang=en" target="_blank">
+                <img class="social-media-logo" src="~/assets/img/twitter-logo.svg" alt="twitter">
+            </a>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <div class="nuxtjsContainer">
+                    <small>Powered by</small>
+                    <a href="https://nuxtjs.org/" target="_blank">
+                        <img id="nuxt-logo" src="https://nuxtjs.org/logos/nuxt-square.svg" alt="nuxtjs">
+                    </a>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
@@ -21,4 +34,29 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.footer {
+    padding-bottom: 0;
+}
+
+.social-media-link {
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+}
+
+.social-media-logo {
+    width: 30px;
+}
+
+.nuxtjsContainer {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.nuxtjsContainer small {
+    margin-right: 0.3rem;
+}
+
+#nuxt-logo{
+    width: 15px;
+}
 </style>
